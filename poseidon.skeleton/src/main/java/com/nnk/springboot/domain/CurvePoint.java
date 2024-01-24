9b0ee7b5f59f3ec04fb6ff82a4223d77c4701e59
atezier <atezier@outlook.fr>
2024-01-24 11:48:59 +0100

    P7 Getter Setter Constructor on Domain class

--- poseidon.skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ poseidon.skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -20,4 +20,57 @@ public class CurvePoint {
     @Column(name = "creationDate")
     Timestamp creationDate;
 
+    public CurvePoint(Integer id, Double term, Double value) {
+        this.id = id;
+        this.term = term;
+        this.value = value;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCurveId() {
+        return curveId;
+    }
+
+    public void setCurveId(Integer curveId) {
+        this.curveId = curveId;
+    }
+
+    public Timestamp getAsOfDate() {
+        return asOfDate;
+    }
+
+    public void setAsOfDate(Timestamp asOfDate) {
+        this.asOfDate = asOfDate;
+    }
+
+    public Double getTerm() {
+        return term;
+    }
+
+    public void setTerm(Double term) {
+        this.term = term;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
 }
--- poseidon.skeleton/src/main/java/com/nnk/springboot/repositories/BidListRepository.java
+++ poseidon.skeleton/src/main/java/com/nnk/springboot/repositories/BidListRepository.java
@@ -2,8 +2,16 @@ package com.nnk.springboot.repositories;
 
 import com.nnk.springboot.domain.BidList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 
 public interface BidListRepository extends JpaRepository<BidList, Integer> {
+
+    BidList save(BidList bidList);
+    BidList findByBidListId(@NonNull Integer bidListId);
+
+    void delete(BidList bidList);
+
+    boolean updateByBidListId(@NonNull Integer bidListId, BidList bidList);
 
 }
